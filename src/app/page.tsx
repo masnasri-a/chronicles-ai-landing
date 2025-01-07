1e6bb57141dd705c0a2979f2a6f990d31fcc0bef
nasriadzlani <nasriadzlani@live.com>
2025-01-07 15:18:50 +0700

    fix: [nasri] [CASE-001] persuratan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
   return (
     <div
-      className="bg-black text-white font-mono flex justify-center items-center flex-col h-screen text-center overflow-hidden"
+      className="bg-black text-white font-mono flex justify-center items-center flex-col h-screen text-center overflow-hidden p-4"
       style={{ 
       backgroundImage: 'url(/data.gif)', 
       backgroundSize: 'cover', 
@@ -50,22 +50,22 @@ export default function Home() {
       }}
     >
       <div id="container" className="animate-fadeIn">
-      <h1 className="text-3xl tracking-widest uppercase mb-5 opacity-90">The Whispers Begin In</h1>
-      <div id="countdown" className="text-4xl font-bold tracking-wide text-teal-400">
+      <h1 className="text-2xl md:text-3xl tracking-widest uppercase mb-5 opacity-90">The Whispers Begin In</h1>
+      <div id="countdown" className="text-3xl md:text-4xl font-bold tracking-wide text-teal-400">
         {timeLeft ? (
         `${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`
         ) : (
         "It has begun."
         )}
       </div>
-      <div className="info-section mt-10 text-lg opacity-80">
+      <div className="info-section mt-10 text-base md:text-lg opacity-80">
         <p>Follow us on Twitter: <a href="https://twitter.com/ECHOChronicles" target="_blank" className="text-teal-400 hover:opacity-70 transition-opacity"> @ECHOChronicles</a></p>
-        <div className="contract mt-2 text-base tracking-wide break-words">
+        <div className="contract mt-2 text-sm md:text-base tracking-wide break-words">
         <p>Contract Address:</p>
-        <p>H3a9yYPJrp8WAqkAFY8jctzPwyGyNowcEsi3rmagpump</p>
+        <p className=" text-xs md:text-base">H3a9yYPJrp8WAqkAFY8jctzPwyGyNowcEsi3rmagpump</p>
         </div>
       </div>
-      <div id="footer" className="mt-5 text-base opacity-60 animate-flicker">Echo Chronicles // 2025</div>
+      <div id="footer" className="mt-5 text-sm md:text-base opacity-60 animate-flicker">Echo Chronicles // 2025</div>
       </div>
     </div>
   );
