52a97e397e1dcf2774e7f7f0acb0a31a4ee3cc75
Nasri Adzlani <nasriadzlani@live.com>
2025-01-09 09:28:48 +0700

    change favicon and GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-E6CZL397P8" />
     </html>
   );
 }
